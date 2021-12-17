786ba908352efdf03040b54f496390d4fcc6a108
shrouq00M <91944817+shrouq00M@users.noreply.github.com>
2021-12-17 16:26:32 +0300

    Create ERC-20.sol

---
+++ ERC-20.sol
@@ -0,0 +1,14 @@
+
+
+ pragma solidity ^0.8.0;
+
+ import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC20/ERC20.sol";
+
+
+contract Group3Token is ERC20 {
+
+ 
+    constructor(string memory name, string memory symbol) ERC20(name, symbol){
+                _mint(msg.sender, (10**24));
+    }
+}
